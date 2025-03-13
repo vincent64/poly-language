b65e3fdc39bb6133039065da37a6db9a8f639694
Vincent Philippe <39349586+vincent64@users.noreply.github.com>
2025-03-14 00:07:31 +0100

    Add javadoc in Object

--- src/poly/compiler/analyzer/type/Object.java
+++ src/poly/compiler/analyzer/type/Object.java
@@ -3,18 +3,28 @@ package poly.compiler.analyzer.type;
 import poly.compiler.resolver.symbol.ClassSymbol;
 
 /**
- * The Object class. This class represents an object type, and contains
- * the full name of its class.
+ * The Object class. This class represents an object type, and contains the
+ * class symbol of the object instance.
+ * @author Vincent Philippe (@vincent64)
  */
 public class Object extends Type {
+    /** The null reference object. */
     public static final Object NULL_REFERENCE = new Object(null);
     private final ClassSymbol classSymbol;
 
+    /**
+     * Constructs an object with the given class symbol.
+     * @param classSymbol the class symbol
+     */
     public Object(ClassSymbol classSymbol) {
         super(Kind.OBJECT);
         this.classSymbol = classSymbol;
     }
 
+    /**
+     * Returns the object class symbol.
+     * @return the class symbol
+     */
     public ClassSymbol getClassSymbol() {
         return classSymbol;
     }
