d54a2a68e3f101ff057c6d0281a19e758c35b0d6
Vincent Philippe <39349586+vincent64@users.noreply.github.com>
2025-03-14 00:07:42 +0100

    Add javadoc in Array

--- src/poly/compiler/analyzer/type/Array.java
+++ src/poly/compiler/analyzer/type/Array.java
@@ -3,18 +3,28 @@ package poly.compiler.analyzer.type;
 import poly.compiler.util.ClassName;
 
 /**
- * The Array class. This class represents an array type, and contains
- * the type of the array.
+ * The Array class. This class represents an array type, and contains the
+ * type of the array.
+ * @author Vincent Philippe (@vincent64)
  */
 public class Array extends Type {
+    /** The array size attribute name. */
     public static final String SIZE = "size";
     private final Type type;
 
+    /**
+     * Constructs an array with the given type.
+     * @param type the type
+     */
     public Array(Type type) {
         super(Kind.ARRAY);
         this.type = type;
     }
 
+    /**
+     * Returns the array type.
+     * @return the type
+     */
     public Type getType() {
         return type;
     }
