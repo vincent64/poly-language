bc77b796e3a18195125ee2da56871238a7e6d089
Vincent Philippe <39349586+vincent64@users.noreply.github.com>
2025-03-20 22:21:01 +0100

    Create Transformer class

---
+++ src/poly/compiler/analyzer/Transformer.java
@@ -0,0 +1,18 @@
+package poly.compiler.analyzer;
+
+import poly.compiler.resolver.symbol.ClassSymbol;
+
+/**
+ * The Transformer class. This class is used by the Analyzer class to transform
+ * some complex parts of the AST. This includes transforming operation overload.
+ * @author Vincent Philippe (@vincent64)
+ */
+public class Transformer {
+    private final Analyzer analyzer;
+    private final ClassSymbol classSymbol;
+
+    public Transformer(Analyzer analyzer, ClassSymbol classSymbol) {
+        this.analyzer = analyzer;
+        this.classSymbol = classSymbol;
+    }
+}
