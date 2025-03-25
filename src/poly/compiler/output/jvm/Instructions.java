74163865a90de699318d7b2cd6069dd61204cd70
Vincent Philippe <39349586+vincent64@users.noreply.github.com>
2025-03-25 23:36:48 +0100

    Remove invalid class access flag in Instructions

--- src/poly/compiler/output/jvm/Instructions.java
+++ src/poly/compiler/output/jvm/Instructions.java
@@ -461,7 +461,6 @@ public class Instructions {
         public static final short SUPER = 0x0020;
         public static final short INTERFACE = 0x0200;
         public static final short ABSTRACT = 0x0400;
-        public static final short INNER = 0x0800;
         public static final short SYNTHETIC = 0x1000;
         public static final short ANNOTATION = 0x2000;
         public static final short ENUM = 0x4000;
