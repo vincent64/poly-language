ce61fd67c4c828f24e7fcd9eef85a934e67a39ae
Vincent Philippe <39349586+vincent64@users.noreply.github.com>
2025-04-12 19:14:36 +0200

    Update version in Compiler

--- src/poly/compiler/Compiler.java
+++ src/poly/compiler/Compiler.java
@@ -17,7 +17,7 @@ import java.util.List;
  */
 public class Compiler {
     /** The compiler current version. */
-    public static final String VERSION = "0.1";
+    public static final String VERSION = "0.2";
 
     /** The compiler starting message. */
     private static final String HEADER = """
