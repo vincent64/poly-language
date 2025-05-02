bac15bfb41a76ac9e046ee31008b1e4623bdba4c
Vincent Philippe <39349586+vincent64@users.noreply.github.com>
2025-05-02 20:09:00 +0200

    Move LineNumberEntry to Entry in LineNumberTable

--- src/poly/compiler/generator/LineNumberTable.java
+++ src/poly/compiler/generator/LineNumberTable.java
@@ -1,7 +1,6 @@
 package poly.compiler.generator;
 
 import poly.compiler.output.Byteable;
-import poly.compiler.output.attribute.LineNumberEntry;
 import poly.compiler.util.ByteArray;
 
 import java.util.ArrayList;
@@ -13,7 +12,7 @@ import java.util.List;
  * @author Vincent Philippe (@vincent64)
  */
 public class LineNumberTable implements Byteable {
-    private final List<LineNumberEntry> entries;
+    private final List<Entry> entries;
     private int currentProgramCounter, currentLineNumber;
 
     /**
@@ -34,7 +33,7 @@ public class LineNumberTable implements Byteable {
      */
     public void addEntry(int programCounter, int lineNumber) {
         if(programCounter > currentProgramCounter && lineNumber > currentLineNumber) {
-            entries.add(new LineNumberEntry((short) programCounter, (short) lineNumber));
+            entries.add(new Entry((short) programCounter, (short) lineNumber));
             currentProgramCounter = programCounter;
             currentLineNumber = lineNumber;
         }
@@ -47,9 +46,39 @@ public class LineNumberTable implements Byteable {
         byteArray.add((short) entries.size());
 
         //Add every entry
-        for(LineNumberEntry entry : entries)
+        for(Entry entry : entries)
             byteArray.add(entry.getBytes());
 
         return byteArray.getBytes();
+    }
+
+    /**
+     * The LineNumberTable.Entry class. This class represents a line number entry, as described
+     * by the JVM specification.
+     */
+    private static class Entry implements Byteable {
+        private final short startProgramCounter;
+        private final short lineNumber;
+
+        /**
+         * Constructs a line number entry with the given starting program counter and line number.
+         * @param startProgramCounter the starting program counter
+         * @param lineNumber the line number
+         */
+        public Entry(short startProgramCounter, short lineNumber) {
+            this.startProgramCounter = startProgramCounter;
+            this.lineNumber = lineNumber;
+        }
+
+        @Override
+        public byte[] getBytes() {
+            ByteArray byteArray = new ByteArray();
+
+            //Add program counter and line number
+            byteArray.add(startProgramCounter);
+            byteArray.add(lineNumber);
+
+            return byteArray.getBytes();
+        }
     }
 }
