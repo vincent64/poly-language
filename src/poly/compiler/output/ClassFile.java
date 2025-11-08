05fa8a5d4d0aa56aa406772f2835a170b1c19765
Vincent Philippe <39349586+vincent64@users.noreply.github.com>
2025-11-08 21:03:29 +0100

    Improve javadoc in ClassFile

--- src/poly/compiler/output/ClassFile.java
+++ src/poly/compiler/output/ClassFile.java
@@ -21,22 +21,24 @@ import java.util.List;
  * The ClassFile class. This class represents a class file and its content,
  * as described by the JVM specification. The class file structure is as follows :
  * <pre>
- *      u4              magic;
- *      u2              minor_version;
- *      u2              major_version;
- *      u2              constant_pool_count;
- *      cp_info         constant_pool[constant_pool_count-1];
- *      u2              access_flags;
- *      u2              this_class;
- *      u2              super_class;
- *      u2              interfaces_count;
- *      u2              interfaces[interfaces_count];
- *      u2              fields_count;
- *      field_info      fields[fields_count];
- *      u2              methods_count;
- *      method_info     methods[methods_count];
- *      u2              attributes_count;
- *      attribute_info  attributes[attributes_count];
+ *      ClassFile {
+ *          u4              magic;
+ *          u2              minor_version;
+ *          u2              major_version;
+ *          u2              constant_pool_count;
+ *          cp_info         constant_pool[constant_pool_count-1];
+ *          u2              access_flags;
+ *          u2              this_class;
+ *          u2              super_class;
+ *          u2              interfaces_count;
+ *          u2              interfaces[interfaces_count];
+ *          u2              fields_count;
+ *          field_info      fields[fields_count];
+ *          u2              methods_count;
+ *          method_info     methods[methods_count];
+ *          u2              attributes_count;
+ *          attribute_info  attributes[attributes_count];
+ *      }
  * </pre>
  * @author Vincent Philippe (@vincent64)
  */
