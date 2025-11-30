f0573fae7e4ef1e00373e2f7635481b426cf0e8d
Vincent Philippe <39349586+vincent64@users.noreply.github.com>
2025-11-30 23:36:16 +0100

    Replace char array with String in Keyword

--- src/poly/compiler/tokenizer/content/Keyword.java
+++ src/poly/compiler/tokenizer/content/Keyword.java
@@ -2,7 +2,8 @@ package poly.compiler.tokenizer.content;
 
 import poly.compiler.tokenizer.Token;
 
-import static poly.compiler.util.Character.isSameString;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * The Keyword class. This class contains every reserved keywords of the Poly language.
@@ -12,76 +13,75 @@ import static poly.compiler.util.Character.isSameString;
  */
 public class Keyword {
     //Primitive keywords
-    public static final char[] PRIMITIVE_BOOLEAN = {'b', 'o', 'o', 'l'};
-    public static final char[] PRIMITIVE_BYTE = {'b', 'y', 't', 'e'};
-    public static final char[] PRIMITIVE_CHAR = {'c', 'h', 'a', 'r'};
-    public static final char[] PRIMITIVE_SHORT = {'s', 'h', 'o', 'r', 't'};
-    public static final char[] PRIMITIVE_INTEGER = {'i', 'n', 't'};
-    public static final char[] PRIMITIVE_LONG = {'l', 'o', 'n', 'g'};
-    public static final char[] PRIMITIVE_FLOAT = {'f', 'l', 'o', 'a', 't'};
-    public static final char[] PRIMITIVE_DOUBLE = {'d', 'o', 'u', 'b', 'l', 'e'};
+    public static final String PRIMITIVE_BOOLEAN = "bool";
+    public static final String PRIMITIVE_BYTE = "byte";
+    public static final String PRIMITIVE_CHAR = "char";
+    public static final String PRIMITIVE_SHORT = "short";
+    public static final String PRIMITIVE_INTEGER = "int";
+    public static final String PRIMITIVE_LONG = "long";
+    public static final String PRIMITIVE_FLOAT = "float";
+    public static final String PRIMITIVE_DOUBLE = "double";
 
     //Expression keywords
-    public static final char[] EXPRESSION_TRUE = {'t', 'r', 'u', 'e'};
-    public static final char[] EXPRESSION_FALSE = {'f', 'a', 'l', 's', 'e'};
-    public static final char[] EXPRESSION_NULL = {'n', 'u', 'l', 'l'};
-    public static final char[] EXPRESSION_THIS = {'t', 'h', 'i', 's'};
-    public static final char[] EXPRESSION_SUPER = {'s', 'u', 'p', 'e', 'r'};
-    public static final char[] EXPRESSION_OUTER = {'o', 'u', 't', 'e', 'r'};
-    public static final char[] EXPRESSION_SUM = {'s', 'u', 'm'};
-    public static final char[] EXPRESSION_PROD = {'p', 'r', 'o', 'd'};
+    public static final String EXPRESSION_TRUE = "true";
+    public static final String EXPRESSION_FALSE = "false";
+    public static final String EXPRESSION_NULL = "null";
+    public static final String EXPRESSION_THIS = "this";
+    public static final String EXPRESSION_SUPER = "super";
+    public static final String EXPRESSION_OUTER = "outer";
+    public static final String EXPRESSION_SUM = "sum";
+    public static final String EXPRESSION_PROD = "prod";
 
     //Statement keywords
-    public static final char[] STATEMENT_IF = {'i', 'f'};
-    public static final char[] STATEMENT_ELSE = {'e', 'l', 's', 'e'};
-    public static final char[] STATEMENT_FOR = {'f', 'o', 'r'};
-    public static final char[] STATEMENT_WHILE = {'w', 'h', 'i', 'l', 'e'};
-    public static final char[] STATEMENT_DO = {'d', 'o'};
-    public static final char[] STATEMENT_RETURN = {'r', 'e', 't', 'u', 'r', 'n'};
-    public static final char[] STATEMENT_SWITCH = {'s', 'w', 'i', 't', 'c', 'h'};
-    public static final char[] STATEMENT_MATCH = {'m', 'a', 't', 'c', 'h'};
-    public static final char[] STATEMENT_CASE = {'c', 'a', 's', 'e'};
-    public static final char[] STATEMENT_ASSERT = {'a', 's', 's', 'e', 'r', 't'};
-    public static final char[] STATEMENT_CONTINUE = {'c', 'o', 'n', 't', 'i', 'n', 'u', 'e'};
-    public static final char[] STATEMENT_BREAK = {'b', 'r', 'e', 'a', 'k'};
-    public static final char[] STATEMENT_FOREACH = {'f', 'o', 'r', 'e', 'a', 'c', 'h'};
-    public static final char[] STATEMENT_TRY = {'t', 'r', 'y'};
-    public static final char[] STATEMENT_CATCH = {'c', 'a', 't', 'c', 'h'};
-    public static final char[] STATEMENT_THROW = {'t', 'h', 'r', 'o', 'w'};
+    public static final String STATEMENT_IF = "if";
+    public static final String STATEMENT_ELSE = "else";
+    public static final String STATEMENT_FOR = "for";
+    public static final String STATEMENT_WHILE = "while";
+    public static final String STATEMENT_DO = "do";
+    public static final String STATEMENT_RETURN = "return";
+    public static final String STATEMENT_SWITCH = "switch";
+    public static final String STATEMENT_MATCH = "match";
+    public static final String STATEMENT_CASE = "case";
+    public static final String STATEMENT_ASSERT = "assert";
+    public static final String STATEMENT_CONTINUE = "continue";
+    public static final String STATEMENT_BREAK = "break";
+    public static final String STATEMENT_FOREACH = "foreach";
+    public static final String STATEMENT_TRY = "try";
+    public static final String STATEMENT_CATCH = "catch";
+    public static final String STATEMENT_THROW = "throw";
 
     //Class keywords
-    public static final char[] CLASS = {'c', 'l', 'a', 's', 's'};
-    public static final char[] CLASS_NEW = {'n', 'e', 'w'};
-    public static final char[] CLASS_INTERFACE = {'i', 'n', 't', 'e', 'r', 'f', 'a', 'c', 'e'};
-    public static final char[] CLASS_ENUM = {'e', 'n', 'u', 'm'};
-    public static final char[] CLASS_INNER = {'i', 'n', 'n', 'e', 'r'};
-    public static final char[] CLASS_EXCEPTION = {'e', 'x', 'c', 'e', 'p', 't', 'i', 'o', 'n'};
-    public static final char[] CLASS_DATUM = {'d', 'a', 't', 'u', 'm'};
+    public static final String CLASS = "class";
+    public static final String CLASS_NEW = "new";
+    public static final String CLASS_INTERFACE = "interface";
+    public static final String CLASS_ENUM = "enum";
+    public static final String CLASS_INNER = "inner";
+    public static final String CLASS_EXCEPTION = "exception";
+    public static final String CLASS_DATUM = "datum";
 
     //Method keywords
-    public static final char[] METHOD = {'f', 'n'};
-    public static final char[] METHOD_VOID = {'v', 'o', 'i', 'd'};
-    public static final char[] METHOD_CONSTRUCTOR = {'c', 'o', 'n', 's', 't', 'r', 'u', 'c', 't', 'o', 'r'};
-    public static final char[] METHOD_OPERATOR = {'o', 'p'};
-    public static final char[] METHOD_EXTERNAL = {'e', 'x', 't'};
-    public static final char[] METHOD_NULLIFIER = {'n', 'u', 'l', 'l', 'i', 'f', 'i', 'e', 'r'};
+    public static final String METHOD = "fn";
+    public static final String METHOD_VOID = "void";
+    public static final String METHOD_CONSTRUCTOR = "constructor";
+    public static final String METHOD_OPERATOR = "op";
+    public static final String METHOD_EXTERNAL = "ext";
+    public static final String METHOD_NULLIFIER = "nullifier";
 
     //Variable keywords
-    public static final char[] VAR_CONST = {'c', 'o', 'n', 's', 't'};
-    public static final char[] VAR_ATTRIBUTE = {'a', 't', 't', 'r'};
+    public static final String VAR_CONST = "const";
+    public static final String VAR_ATTRIBUTE = "attr";
 
     //Header keywords
-    public static final char[] PACKAGE = {'p', 'a', 'c', 'k', 'a', 'g', 'e'};
-    public static final char[] IMPORT = {'i', 'm', 'p', 'o', 'r', 't'};
-    public static final char[] DEFINE = {'d', 'e', 'f', 'i', 'n', 'e'};
+    public static final String PACKAGE = "package";
+    public static final String IMPORT = "import";
+    public static final String DEFINE = "define";
 
     //Concurrency keywords (reserved for later use)
-    public static final char[] CONCURRENCY_ASYNC = {'a', 's', 'y', 'n', 'c'};
-    public static final char[] CONCURRENCY_AWAIT = {'a', 'w', 'a', 'i', 't'};
-    public static final char[] CONCURRENCY_THREAD = {'t', 'h', 'r', 'e', 'a', 'd'};
+    public static final String CONCURRENCY_ASYNC = "async";
+    public static final String CONCURRENCY_AWAIT = "await";
+    public static final String CONCURRENCY_THREAD = "thread";
 
-    //All keywords
-    private static final char[][] ALL = {
+    private static final Set<String> ALL = new HashSet<>(Set.of(
             PRIMITIVE_BOOLEAN, PRIMITIVE_BYTE, PRIMITIVE_CHAR, PRIMITIVE_SHORT,
             PRIMITIVE_INTEGER, PRIMITIVE_LONG, PRIMITIVE_FLOAT, PRIMITIVE_DOUBLE,
             EXPRESSION_TRUE, EXPRESSION_FALSE, EXPRESSION_NULL, EXPRESSION_SUPER, EXPRESSION_THIS,
@@ -95,12 +95,12 @@ public class Keyword {
             VAR_CONST, VAR_ATTRIBUTE,
             PACKAGE, IMPORT, DEFINE,
             CONCURRENCY_ASYNC, CONCURRENCY_AWAIT, CONCURRENCY_THREAD
-    };
+    ));
 
-    private static final char[][] PRIMITIVE = {
+    private static final Set<String> PRIMITIVE = new HashSet<>(Set.of(
             PRIMITIVE_BOOLEAN, PRIMITIVE_BYTE, PRIMITIVE_CHAR, PRIMITIVE_SHORT,
             PRIMITIVE_INTEGER, PRIMITIVE_LONG, PRIMITIVE_FLOAT, PRIMITIVE_DOUBLE
-    };
+    ));
 
     /**
      * Returns whether the given token is a keyword token.
@@ -117,11 +117,16 @@ public class Keyword {
      * @return true if the content is a keyword
      */
     public static boolean isKeyword(char[] content) {
-        for(char[] keyword : ALL) {
-            if(isSameString(content, keyword)) return true;
-        }
+        return isKeyword(String.valueOf(content));
+    }
 
-        return false;
+    /**
+     * Returns whether the given content is a keyword.
+     * @param content the content
+     * @return true if the content is a keyword
+     */
+    public static boolean isKeyword(String content) {
+        return ALL.contains(content);
     }
 
     /**
@@ -138,11 +143,7 @@ public class Keyword {
      * @param content the content
      * @return true if the content is a keyword
      */
-    public static boolean isPrimitiveKeyword(char[] content) {
-        for(char[] keyword : PRIMITIVE) {
-            if(isSameString(content, keyword)) return true;
-        }
-
-        return false;
+    public static boolean isPrimitiveKeyword(String content) {
+        return PRIMITIVE.contains(content);
     }
 }
